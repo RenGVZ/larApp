518f16339d0ec4cfd5a7af568e7a33cb1fe77faf
Gavin Garza <48096041+RenGVZ@users.noreply.github.com>
2020-09-19 11:26:54 +0900

    Revert "Revert "added bootstrap to project and added a navbar with page-switching features""
    This reverts commit e0b6a904cd90bafb36c1950ef905982584b0ed2f.

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,13 +1,9 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-  <head>
-      <meta charset="utf-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1">
-      <title>{{config('app.name', 'larApp')}}</title>
-      <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-  </head>
-  <body>
-    <h1>Welcome to Laravel</h1>
+@extends('layouts.app')
+@section('content')
+  <div class="jumbotron text-center">
+    <h1>{{$title}}!</h1>
     <p>This is a Laravel application from scratch</p>
-  </body>
-</html>
+    <p><a href="/login" class="btn btn-primary btn-lg" role="button">Login</a> <a href="/login" class="btn btn-success btn-lg" role="button">Register</a></p>
+  </div>
+@endsection
+
